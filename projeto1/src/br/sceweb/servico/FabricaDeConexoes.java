e23b13925dd70ca4237a4cd7b5bd4467f2ff6d00
Lab103 <Lab103@LAB101-21>
2017-09-22 15:41:26 -0300

    versao inicial

---
+++ projeto1/src/br/sceweb/servico/FabricaDeConexoes.java
@@ -0,0 +1,16 @@
+package br.sceweb.servico;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+public class FabricaDeConexoes {
+	public Connection getConnection() {
+		String url = "jdbc:mysql://localhost/sceweb";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return (Connection) DriverManager.getConnection(url, "root", "alunofatec");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
