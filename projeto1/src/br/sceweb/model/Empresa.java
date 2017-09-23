59a2b7fe7bba77b7754ce5f784eda6d19b088186
Lab103 <Lab103@LAB101-21>
2017-09-23 11:21:14 -0300

    inclusao de empresa

--- projeto1/src/br/sceweb/model/Empresa.java
+++ projeto1/src/br/sceweb/model/Empresa.java
@@ -1,8 +1,55 @@
 package br.sceweb.model;
 
 public class Empresa {
-String cnpj;
-String nomeDaEmpresa;
-String nomeFantasia;
+	String cnpj;
+	String nomeDaEmpresa;
+	String nomeFantasia;
+	String endereco;
+	String telefone;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+
+	}
+
+	public String getNomeDaEmpresa() {
+		return nomeDaEmpresa;
+	}
+
+	public void setNomeDaEmpresa(String nomeDaEmpresa) {
+		if (nomeDaEmpresa.equals("")) {
+			throw new IllegalArgumentException("Nome da empresa inválido!");
+		} else
+			this.nomeDaEmpresa = nomeDaEmpresa;
+
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 
 }
